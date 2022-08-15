515df3f4d8d0a068aab2ddbe5fa206c219681171
LEXASPB78 <ashkreba@yandex.ru>
2022-08-15 11:43:35 +0300

    Reshenie23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт
+// таблицу кубов чисел от 1 до N.
+
+void kyb (int number)
+{
+    int index = 1;
+    while (index < number)
+    {
+        Console.Write($"{index*index*index}, ");
+        index++;
+    }
+    Console.Write($"{index*index*index} " );
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+kyb(num);
